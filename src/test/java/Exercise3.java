1fb75e6826b2b7dde93278b178576dc89735a312
eliasekelow <ekelow.elias@gmail.com>
2026-04-22 13:04:22 +0200

    ah

--- src/test/java/Exercise3.java
+++ src/test/java/Exercise3.java
@@ -3,13 +3,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.io.*;
 
 public class Exercise3 {
 
 	private final List<Recording> recordings = new ArrayList<>();
 
 	public void exportRecordings(String fileName) {
-
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //vi skapar recording objekt och stoppar in dom i recordings
+            }
+            fileReader.close();
+            reader.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.printf( "%s file not found", fileName);
+        }
+        catch (IOException e){
+            System.out.printf( "%s file not found", fileName);
+        }
 	}
 
 	public void importRecordings(String fileName) {
