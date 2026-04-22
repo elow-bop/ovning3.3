2f4cc2948df63f89a94cf10afd185b2db290c51f
eliasekelow <ekelow.elias@gmail.com>
2026-04-22 14:35:27 +0200

    ah

--- src/test/java/Exercise3.java
+++ src/test/java/Exercise3.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.io.*;
 
 public class Exercise3 {
@@ -16,10 +17,25 @@ public class Exercise3 {
         try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader reader = new BufferedReader(fileReader);
+            int numberOfRecords = Integer.parseInt(reader.readLine()); //första raden i textfilen (recording_input) säger hur många olika records som filen innehåller - ee
             String line;
-            while ((line = reader.readLine()) != null) {
-                //vi skapar recording objekt och stoppar in dom i recordings
+
+            for (int i = 0; i>numberOfRecords; i++){ //för varje record som finns i recording_input så skapar vi ett recording objekt - ee
+                line = reader.readLine();
+                String [] recordingInfo = line.split(";"); //vi splittar vår inlästa line för att dela upp artistnamn, albumnamn och år - ee
+
+                //Set <String> genres = new skapa ett set här som vi kan skicka in i parametern när vi skapar en recording - ee
+                int numberOfGenres = Integer.parseInt(reader.readLine()); //nästa rad i recording_input kommer säga hur många genres som recordingen tillhör - ee
+                for (int ii = 0; ii>numberOfGenres; ii++){
+                    line = reader.readLine();
+                }
+
             }
+
+            //while ((line = reader.readLine()) != null) {
+                //vi skapar recording objekt och stoppar in dom i recordings
+                //Recording r = new Recording();
+            //}
             fileReader.close();
             reader.close();
         }
