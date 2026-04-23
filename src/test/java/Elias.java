8846cba2308de957928699360171d06f63472f76
eliasekelow <ekelow.elias@gmail.com>
2026-04-23 11:07:30 +0200

    ah

--- src/test/java/Elias.java
+++ src/test/java/Elias.java
@@ -1,2 +1,8 @@
 public class Elias {
+    public static void main (String [] arfs){
+        Exercise3 exercise = new Exercise3();
+        exercise.importRecordings("src/recording_input.txt");
+        System.out.print(exercise.getRecordings());
+
+    }
 }
